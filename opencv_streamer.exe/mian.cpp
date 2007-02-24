4304dea7eb49c34548cd8d0147cb28f2d4a3a68e
andrea.carbone <andrea.carbone@1ffd000b-a628-0410-9a29-793f135cad17>
2007-02-24 11:09:32 +0000

    opencv_streamer.exe progetto

---
+++ opencv_streamer.exe/mian.cpp
@@ -0,0 +1,26 @@
+#include <alcor/core/stream_server_t.hpp>
+#include "alcor/sense/opencv_data_source_t.h"
+
+using namespace all;
+
+int main() {
+
+  sense::opencv_data_source_t m_source;
+
+  //m_source.set_quality(50);
+
+	all::core::stream_server_t server(m_source,"config/stream_server.ini");
+
+	server.run_async();
+
+	server.start_streaming();
+
+	getchar();
+
+	server.stop_streaming();
+
+	server.stop();
+
+	getchar();
+
+} 
