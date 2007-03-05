e048bc06b4574728a1d35ef9f18235a3ab4a7b09
andrea.carbone <andrea.carbone@1ffd000b-a628-0410-9a29-793f135cad17>
2007-03-05 02:35:09 +0000

    modificati i progetti di Stefano per farli compilare .. ma è da mettersi d'acoordo su dove stanno le wx ... Inoltre ho dovuto modificare i sorgenti di wxCtb per farli compilare ..
    Draft iniziale per p3_gateway ... il gateway solo controllo del robot senza server ... pimplato, lightweight e senza alcuna dipendenza da Aria (per chi lo usa).
    
    Il server/client .. sarà fatto con client_server delle boost

--- trunk/trm/trm_app_inc.h
+++ trunk/trm/trm_app_inc.h
@@ -1,6 +1,6 @@
 #pragma once //trm_app_inc.h
 //-----------------------------------------------------------------------++
-#include "alcor/act/p3dx_client_t.h"
+#include "alcor/act/p3dx_gateway.h"
 #include "alcor/act/p3_odometry_adapter_t.h"
 #include "alcor/act/directed_perception_ptu_t.h"
 #include "alcor/act/pantilt_control_loop_t.hpp"
